91be4b9b209dccf45b2aa27b1c4e758a4ef2da41
Weijie Jiang <115952413+Weijie98-code@users.noreply.github.com>
2024-04-12 22:03:17 -0400

    Update scoreboard.js

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -32,7 +32,8 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+        //increase score if the button is pressed
+        display_scoreboard(result.scoreboard);
     },
     error: function(request, status, error){
         console.log("Error");
